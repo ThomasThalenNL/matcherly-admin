d9e113a4ba4de5b9cad68d797ab97a18bfa1ce06
Thomas Thalen <thomas@matcherly.com>
2026-02-04 15:24:31 +0000

    chore: initialize matcherly-admin project structure

---
+++ app/kanban/page.tsx
@@ -0,0 +1,9 @@
+// app/kanban/page.tsx
+export default function Kanban() {
+  return (
+    <main>
+      <h1>Kanban board werkt!</h1>
+      <p>(Je ziet deze testpagina nu op /kanban)</p>
+    </main>
+  );
+}
